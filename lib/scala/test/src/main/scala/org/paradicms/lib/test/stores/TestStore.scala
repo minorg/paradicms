2d58b2af78066d0ddee6e3c7eaf04060ef9cb80c
Minor G <minorg@server.fake>
2020-01-26 18:05:43 -0500

    fix TestStore build

--- lib/scala/test/src/main/scala/org/paradicms/lib/test/stores/TestStore.scala
+++ lib/scala/test/src/main/scala/org/paradicms/lib/test/stores/TestStore.scala
@@ -6,25 +6,27 @@ import org.paradicms.service.lib.generic.models.domain
 import org.paradicms.service.lib.generic.models.domain.{Collection, Institution, ObjectSearchResult, User}
 
 class TestStore extends Store {
-  override def getCollectionByUri(collectionUri: Uri, currentUserUri: Option[Uri]): Collection = if (collectionUri == TestData.collection.uri) TestData.collection else throw new NoSuchElementException
+  private val testData = new GenericTestData
 
-  override def getCollectionObjects(collectionUri: Uri, currentUserUri: Option[Uri], limit: Int, offset: Int): List[domain.Object] = if (offset == 0) List(TestData.object_) else List()
+  override def getCollectionByUri(collectionUri: Uri, currentUserUri: Option[Uri]): Collection = if (collectionUri == testData.collection.uri) testData.collection else throw new NoSuchElementException
+
+  override def getCollectionObjects(collectionUri: Uri, currentUserUri: Option[Uri], limit: Int, offset: Int): List[domain.Object] = if (offset == 0) List(testData.object_) else List()
 
   override def getCollectionObjectsCount(collectionUri: Uri, currentUserUri: Option[Uri]): Int = 1
 
-  override def getInstitutionByUri(currentUserUri: Option[Uri], institutionUri: Uri): Institution = if (institutionUri == TestData.institution.uri) TestData.institution else throw new NoSuchElementException
+  override def getInstitutionByUri(currentUserUri: Option[Uri], institutionUri: Uri): Institution = if (institutionUri == testData.institution.uri) testData.institution else throw new NoSuchElementException
 
-  override def getInstitutionCollections(currentUserUri: Option[Uri], institutionUri: Uri): List[Collection] = List(TestData.collection)
+  override def getInstitutionCollections(currentUserUri: Option[Uri], institutionUri: Uri): List[Collection] = List(testData.collection)
 
-  override def getInstitutions(currentUserUri: Option[Uri]): List[Institution] = List(TestData.institution)
+  override def getInstitutions(currentUserUri: Option[Uri]): List[Institution] = List(testData.institution)
 
-  override def getMatchingObjects(currentUserUri: Option[Uri], limit: Int, offset: Int, text: String): List[ObjectSearchResult] = if (offset == 0) List(new ObjectSearchResult(collection = TestData.collection, institution = TestData.institution, object_ = TestData.object_)) else List()
+  override def getMatchingObjects(currentUserUri: Option[Uri], limit: Int, offset: Int, text: String): List[ObjectSearchResult] = if (offset == 0) List(new ObjectSearchResult(collection = testData.collection, institution = testData.institution, object_ = testData.object_)) else List()
 
   override def getMatchingObjectsCount(currentUserUri: Option[Uri], text: String) = 1
 
-  override def getObjectByUri(currentUserUri: Option[Uri], objectUri: Uri): domain.Object = if (objectUri == TestData.object_.uri) TestData.object_ else throw new NoSuchElementException
+  override def getObjectByUri(currentUserUri: Option[Uri], objectUri: Uri): domain.Object = if (objectUri == testData.object_.uri) testData.object_ else throw new NoSuchElementException
 
-  override def getUserByUri(userUri: Uri): Option[User] = if (userUri == TestData.user.uri) Some(TestData.user) else None
+  override def getUserByUri(userUri: Uri): Option[User] = if (userUri == testData.user.uri) Some(testData.user) else None
 
   override def putUser(user: User): Unit = throw new UnsupportedOperationException
 }
