cbb319ceb08bc96a40c2d912a9e474bc3c060868
Minor G <minorg@server.fake>
2020-03-22 16:32:35 -0400

    fix footer

--- app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
+++ app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
@@ -1,7 +1,6 @@
-import {Hrefs} from "paradicms/app/generic/Hrefs";
+import { Hrefs } from "paradicms/app/generic/Hrefs";
 import * as React from "react";
-import {Link} from "react-router-dom";
-import {Col, Container, Row} from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 
 export class Footer extends React.Component {
   render() {
@@ -11,16 +10,16 @@ export class Footer extends React.Component {
           <Row>
             <Col className="text-center" xs="12">
               <a href={Hrefs.contact}>Contact</a>&nbsp;|&nbsp;
-              <Link to={Hrefs.privacy}>Privacy</Link>
             </Col>
           </Row>
           <Row>&nbsp;</Row>
           <Row>
             <Col className="text-center" xs="12">
               <p className="muted">
-                &copy; Copyright 2019,&nbsp;
-                <a href="https://minorgordon.net">Minor Gordon</a>. All rights
-                reserved
+                This site is for educational purposes only.
+              </p>
+              <p>
+                Powered by <a href="https://github.com/minorg/paradicms">paradicms</a>
               </p>
             </Col>
           </Row>
