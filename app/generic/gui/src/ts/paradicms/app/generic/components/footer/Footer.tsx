00ae3feabb5dd93fbe83b09e16184ca2f6e9490f
Minor G <minorg@server.fake>
2020-03-22 16:43:53 -0400

    fix footer again

--- app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
+++ app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
@@ -9,17 +9,15 @@ export class Footer extends React.Component {
         <Container fluid>
           <Row>
             <Col className="text-center" xs="12">
-              <a href={Hrefs.contact}>Contact</a>&nbsp;|&nbsp;
-            </Col>
-          </Row>
-          <Row>&nbsp;</Row>
-          <Row>
-            <Col className="text-center" xs="12">
               <p className="muted">
                 This site is for educational purposes only.
               </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center" xs="12">
               <p>
-                Powered by <a href="https://github.com/minorg/paradicms">paradicms</a>
+                <a href={Hrefs.contact}>Contact</a>&nbsp;|&nbsp;<a href={Hrefs.privacy}>Privacy</a>&nbsp;|&nbsp;<a href="https://github.com/minorg/paradicms">GitHub</a>
               </p>
             </Col>
           </Row>
