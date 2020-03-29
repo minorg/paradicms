201aab06bc564839817b2212d4174efc8f3a8bc6
Minor G <minorg@server.fake>
2020-03-29 14:38:18 -0400

    TestSparqlStore working

--- lib/scala/generic/src/test/scala/org/paradicms/lib/generic/stores/sparql/TestSparqlStore.scala
+++ lib/scala/generic/src/test/scala/org/paradicms/lib/generic/stores/sparql/TestSparqlStore.scala
@@ -1,29 +1,36 @@
 package org.paradicms.lib.generic.stores.sparql
 
 import io.lemonlabs.uri.Uri
-import org.apache.jena.query.{Query, QueryExecution}
-import org.apache.jena.rdfconnection.RDFConnection
+import org.apache.jena.query.{DatasetFactory, Query, QueryExecution, QueryExecutionFactory}
+import org.apache.jena.rdfconnection.{RDFConnection, RDFConnectionFactory}
+import org.apache.jena.riot.{Lang, RDFDataMgr}
 
+/**
+ * Store that reads a .ttl in test/resources into a Dataset and queries it with SPARQL.
+ *
+ * This is a class instead of an object so that it can be "reset" by creating a new instance.
+ */
 abstract class TestSparqlStore extends SparqlConnectionLoanPatterns {
+  private val dataset = DatasetFactory.create()
+  RDFDataMgr.read(dataset, getClass.getResourceAsStream("/test_data.ttl"), Lang.TURTLE)
+
   val currentUserUri = Some(Uri.parse("http://example.com/user"))
 
   protected final def withQueryExecution[T](query: Query)(f: (QueryExecution) => T): T = {
-    throw new UnsupportedOperationException
-//    val queryExecution = QueryExecutionFactory.sparqlService(configuration.sparqlQueryUrl.toString(), query)
-//    try {
-//      f(queryExecution)
-//    } finally {
-//      queryExecution.close()
-//    }
+    val queryExecution = QueryExecutionFactory.create(query, dataset)
+    try {
+      f(queryExecution)
+    } finally {
+      queryExecution.close()
+    }
   }
 
   protected final def withRdfConnection[T]()(f: (RDFConnection) => T): T = {
-    throw new UnsupportedOperationException
-//    val rdfConnection = RDFConnectionFactory.connectFuseki(configuration.sparqlUpdateUrl.toString())
-//    try {
-//      f(rdfConnection)
-//    } finally {
-//      rdfConnection.close()
-//    }
+    val rdfConnection = RDFConnectionFactory.connect(dataset)
+    try {
+      f(rdfConnection)
+    } finally {
+      rdfConnection.close()
+    }
   }
 }
