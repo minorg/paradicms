1468ac8758610fa4cc39b25f87cfe8b56f63d1c8
Minor G <minorg@server.fake>
2020-04-18 20:00:53 -0400

    model separate facets query

--- lib/scala/generic/src/main/scala/org/paradicms/lib/generic/stores/ObjectStore.scala
+++ lib/scala/generic/src/main/scala/org/paradicms/lib/generic/stores/ObjectStore.scala
@@ -1,10 +1,12 @@
 package org.paradicms.lib.generic.stores
 
 import io.lemonlabs.uri.Uri
-import org.paradicms.lib.generic.models.domain.{Collection, Object}
+import org.paradicms.lib.generic.models.domain.{Collection, Institution, Object}
 
 trait ObjectStore {
-  def getObjects(currentUserUri: Option[Uri], limit: Int, offset: Int, query: ObjectQuery, cachedCollectionsByUri: Map[Uri, Collection] = Map()): GetObjectsResult
+  def getObjects(currentUserUri: Option[Uri], limit: Int, offset: Int, query: ObjectQuery, cachedCollectionsByUri: Map[Uri, Collection] = Map(), cachedInstitutionsByuri: Map[Uri, Institution] = Map()): GetObjectsResult
+
+  def getObjectFacets(currentUserUri: Option[Uri], query: ObjectQuery, cachedCollectionsByUri: Map[Uri, Collection] = Map(), cachedInstitutionsByUri: Map[Uri, Institution] = Map()): GetObjectFacetsResult
 
   def getObjectsCount(currentUserUri: Option[Uri], query: ObjectQuery): Int
 
