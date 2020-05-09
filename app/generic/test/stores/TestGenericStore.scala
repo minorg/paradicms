294601d636b1cefb748033ed7cce286849e122be
Minor G <minorg@server.fake>
2020-05-08 20:47:42 -0400

    new GenericTestController that uses a test store

--- app/generic/test/stores/TestGenericStore.scala
+++
@@ -1,8 +0,0 @@
-package stores
-
-import org.paradicms.lib.generic.stores.sparql._
-import org.slf4j.LoggerFactory
-
-final class TestGenericStore extends TestSparqlStore with GenericStore with SparqlCollectionStore with SparqlInstitutionStore with SparqlObjectStore with SparqlUserStore {
-  val logger = LoggerFactory.getLogger(classOf[TestGenericStore])
-}
