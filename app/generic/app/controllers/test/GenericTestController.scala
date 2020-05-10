5b40e2be6f736f8163f36bae1f301633fa24c3e6
Minor G <minorg@server.fake>
2020-05-09 20:35:43 -0400

    allow front end resolution to work on prod files

--- app/generic/app/controllers/test/GenericTestController.scala
+++ app/generic/app/controllers/test/GenericTestController.scala
@@ -4,15 +4,29 @@ import java.io.File
 
 import controllers.{Assets, AssetsFinder}
 import javax.inject.{Inject, Singleton}
-import play.api.Environment
 import play.api.mvc.InjectedController
+import play.api.{Environment, Mode}
 import stores.{GenericStore, TestGenericStore}
 
 @Singleton
 class GenericTestController @Inject()(assets: Assets, assetsFinder: AssetsFinder, environment: Environment, store: GenericStore) extends InjectedController {
   def frontEndPath(path: String) = {
-    val file = new File(s"${environment.rootPath}${assetsFinder.assetsBasePath}${File.separator}${path}").getAbsoluteFile
-    if (file.exists) {
+    val pathExists =
+      if (environment.mode == Mode.Prod) {
+        val streamPath = "/public" + path
+        val stream = getClass.getClassLoader.getResourceAsStream(streamPath)
+        if (stream != null) {
+          stream.close()
+          true
+        } else {
+          false
+        }
+      } else {
+        val file = new File(s"${environment.rootPath}${assetsFinder.assetsBasePath}${File.separator}${path}").getAbsoluteFile
+        file.exists
+      }
+
+    if (pathExists) {
       assets.at("/public", path, aggressiveCaching = false)
     } else {
       assets.at("/public", "index.html", aggressiveCaching = false)
