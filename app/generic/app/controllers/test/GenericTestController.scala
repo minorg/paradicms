f2acf2a41e010aa162bd49cdce184d856b4940ca
Minor G <minorg@server.fake>
2020-05-09 20:08:09 -0400

    support React index.html routes

--- app/generic/app/controllers/test/GenericTestController.scala
+++ app/generic/app/controllers/test/GenericTestController.scala
@@ -1,15 +1,26 @@
 package controllers.test
 
+import java.io.File
+
+import controllers.{Assets, AssetsFinder}
 import javax.inject.{Inject, Singleton}
+import play.api.Environment
 import play.api.mvc.InjectedController
 import stores.{GenericStore, TestGenericStore}
 
 @Singleton
-class GenericTestController @Inject()(store: GenericStore) extends InjectedController {
+class GenericTestController @Inject()(assets: Assets, assetsFinder: AssetsFinder, environment: Environment, store: GenericStore) extends InjectedController {
+  def frontEndPath(path: String) = {
+    val file = new File(s"${environment.rootPath}${assetsFinder.assetsBasePath}${File.separator}${path}").getAbsoluteFile
+    if (file.exists) {
+      assets.at("/public", path, aggressiveCaching = false)
+    } else {
+      assets.at("/public", "index.html", aggressiveCaching = false)
+    }
+  }
+
   def reset = Action { request =>
-    if (request.headers.hasHeader("X-Forwarded-For")) {
-      Forbidden("proxy requests not allowed")
-    } else if (!store.isInstanceOf[TestGenericStore]) {
+    if (!store.isInstanceOf[TestGenericStore]) {
       InternalServerError("test store not enabled");
     } else {
       store.asInstanceOf[TestGenericStore].reset()
