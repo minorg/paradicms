a8a42ed6425b30503b8e054143ec8d24c0431b59
Minor G <minorg@server.fake>
2020-05-16 20:47:46 -0400

    convert Footer

--- app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
+++ app/generic/gui/src/ts/paradicms/app/generic/components/footer/Footer.tsx
@@ -1,28 +1,33 @@
 import { Hrefs } from "paradicms/app/generic/Hrefs";
 import * as React from "react";
-import { Col, Container, Row } from "reactstrap";
+import { Grid, makeStyles } from "@material-ui/core";
 
-export class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <Container fluid>
-          <Row>
-            <Col className="text-center" xs="12">
-              <p className="muted">
-                This site is for educational purposes only.
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center" xs="12">
-              <p>
-                <a href={Hrefs.contact}>Contact</a>&nbsp;|&nbsp;<a href={Hrefs.privacy}>Privacy</a>&nbsp;|&nbsp;<a href="https://github.com/minorg/paradicms">GitHub</a>
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    );
+const useStyles = makeStyles((theme) => ({
+  "footerParagraph": {
+    textAlign: "center"
   }
+}));
+
+export const Footer: React.FunctionComponent = () => {
+  const classes = useStyles();
+  return (
+    <footer>
+      <Grid container>
+        <Grid container>
+          <Grid item xs={12}>
+            <p className="footerParagraph">
+              This site is for educational purposes only.
+            </p>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <p className={classes.footerParagraph}>
+              <a href={Hrefs.contact}>Contact</a>&nbsp;|&nbsp;<a href={Hrefs.privacy}>Privacy</a>&nbsp;|&nbsp;<a href="https://github.com/minorg/paradicms">GitHub</a>
+            </p>
+          </Grid>
+        </Grid>
+      </Grid>
+    </footer>
+  );
 }
