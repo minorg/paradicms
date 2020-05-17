5dbedc395d1ca3bbedc9617a442d36e75e4e9b0c
Minor G <minorg@server.fake>
2020-05-17 16:35:11 -0400

    convert RightsTable

--- app/generic/gui/src/ts/paradicms/app/generic/components/rights/RightsTable.tsx
+++ app/generic/gui/src/ts/paradicms/app/generic/components/rights/RightsTable.tsx
@@ -1,44 +1,63 @@
 import { CollectionOverviewInitialQuery_collectionByUri_rights } from "paradicms/app/generic/api/queries/types/CollectionOverviewInitialQuery";
 import * as React from "react";
 import { Uris } from "@paradicms/base";
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@material-ui/core";
 
 type Rights = CollectionOverviewInitialQuery_collectionByUri_rights;
+
+const useStyles = makeStyles((theme) => ({
+  cell: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
+  },
+  table: {
+    width: "100%"
+  }
+}));
 
 export const RightsTable: React.FunctionComponent<{className?: string; rights: Rights}> = ({
   className,
   rights,
-}) => (
-  <table className={className + " w-100"}>
-    <tbody>
-    {(rights.text || rights.statementUri) ?
-      <tr>
-        <td className="px-2">
-          <strong>Rights</strong>
-        </td>
-        <td className="px-2">{(rights.text && rights.statementUri) ? <a href={rights.statementUri}>{rights.text}</a> : <React.Fragment>{rights.text}</React.Fragment>}</td>
-      </tr> : null}
-      {rights.holder ? (
-        <tr>
-          <td className="px-2">
-            <strong>Holder</strong>
-          </td>
-          <td className="px-2">{rights.holder}</td>
-        </tr>
-      ) : null}
-      {rights.license ? (
-        <tr>
-          <td className="px-2">
-            <strong>License</strong>
-          </td>
-          <td className="px-2">
-            {Uris.isUrl(rights.license) ? (
-              <a href={rights.license}>{rights.license}</a>
-            ) : (
-              <React.Fragment>{rights.license}</React.Fragment>
-            )}
-          </td>
-        </tr>
-      ) : null}
-    </tbody>
-  </table>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table}>
+        <TableBody>
+          {(rights.text || rights.statementUri) ?
+            <TableRow>
+              <TableCell className={classes.cell}>
+                <strong>Rights</strong>
+              </TableCell>
+              <TableCell className={classes.cell}>{(rights.text && rights.statementUri) ?
+                <a href={rights.statementUri}>{rights.text}</a> :
+                <React.Fragment>{rights.text}</React.Fragment>}</TableCell>
+            </TableRow> : null}
+          {rights.holder ? (
+            <TableRow>
+              <TableCell className={classes.cell}>
+                <strong>Holder</strong>
+              </TableCell>
+              <TableCell className={classes.cell}>{rights.holder}</TableCell>
+            </TableRow>
+          ) : null}
+          {rights.license ? (
+            <TableRow>
+              <TableCell className={classes.cell}>
+                <strong>License</strong>
+              </TableCell>
+              <TableCell className={classes.cell}>
+                {Uris.isUrl(rights.license) ? (
+                  <a href={rights.license}>{rights.license}</a>
+                ) : (
+                  <React.Fragment>{rights.license}</React.Fragment>
+                )}
+              </TableCell>
+            </TableRow>
+          ) : null}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
