4501b098a66e7a5ec17afb3d99b26b369700c8bb
Minor G <minorg@server.fake>
2020-07-30 22:17:20 -0400

    incantations in various configuration files that elide webpack and use tsc directly, to get a gatsby monorepo

--- lib/ts/gatsby-material-ui/src/components/Footer.tsx
+++ lib/ts/gatsby-material-ui/src/components/Footer.tsx
@@ -1,79 +1,45 @@
 import * as React from "react";
 import {Grid, makeStyles} from "@material-ui/core";
-import {graphql, StaticQuery} from "gatsby";
 
 const useStyles = makeStyles(theme => ({
-  footerParagraph: {
-    textAlign: "center",
-  },
+  footerParagraph: {},
 }));
 
-export const Footer: React.FunctionComponent = () => {
+export const Footer: React.FunctionComponent<{
+  author: {
+    email: string;
+    name: string;
+    url: string;
+  };
+  gitHubUrl: string;
+}> = ({author, gitHubUrl}) => {
   const classes = useStyles();
   return (
-    <StaticQuery<GatsbyTypes.IndexLayoutQueryQuery>
-      query={graphql`
-        query IndexLayoutQuery {
-          site {
-            siteMetadata {
-              author {
-                email
-                name
-                url
-              }
-              gitHubUrl
-            }
-          }
-        }
-      `}
-      render={(data: {
-        site: {
-          siteMetadata: {
-            author: {
-              email: string;
-              name: string;
-              url: string;
-            };
-            gitHubUrl: string;
-          };
-        };
-      }) => (
-        <footer>
-          <Grid container direction="column">
-            <Grid item>
-              <p className={classes.footerParagraph}>
-                This site is for educational purposes only.
-              </p>
-            </Grid>
-            <Grid item>
-              <p className={classes.footerParagraph}>
-                <a
-                  data-cy="contact-link"
-                  href={"mailto:" + data.site.siteMetadata.author.email}
-                >
-                  Contact
-                </a>
-                &nbsp;|&nbsp;
-                <a
-                  data-cy="github-link"
-                  href={data.site.siteMetadata.gitHubUrl}
-                >
-                  GitHub
-                </a>
-              </p>
-            </Grid>
-            <Grid item>
-              <p className={classes.footerParagraph}>
-                &copy; Copyright 2020,&nbsp;
-                <a href={data.site.siteMetadata.author.url}>
-                  {data.site.siteMetadata.author.name}
-                </a>
-                . All rights reserved
-              </p>
-            </Grid>
-          </Grid>
-        </footer>
-      )}
-    />
+    <footer>
+      <Grid container direction="column">
+        <Grid item>
+          <p className={classes.footerParagraph}>
+            This site is for educational purposes only.
+          </p>
+        </Grid>
+        <Grid item>
+          <p className={classes.footerParagraph}>
+            <a data-cy="contact-link" href={"mailto:" + author.email}>
+              Contact
+            </a>
+            &nbsp;|&nbsp;
+            <a data-cy="github-link" href={gitHubUrl}>
+              GitHub
+            </a>
+          </p>
+        </Grid>
+        <Grid item>
+          <p className={classes.footerParagraph}>
+            &copy; Copyright 2020,&nbsp;
+            <a href={author.url}>{author.name}</a>. All rights reserved
+          </p>
+        </Grid>
+      </Grid>
+    </footer>
   );
 };
