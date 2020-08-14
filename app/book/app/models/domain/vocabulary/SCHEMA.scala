59ca8a657055cbcd5fd87f3763a73af84322810b
Minor G <minorg@server.fake>
2020-08-14 08:44:14 -0400

    delete app/book, never finished

--- app/book/app/models/domain/vocabulary/SCHEMA.scala
+++
@@ -1,9 +0,0 @@
-package models.domain.vocabulary
-
-import org.apache.jena.rdf.model.ResourceFactory
-
-object SCHEMA {
-  val URI = "http://schema.org/"
-
-  val pageCount = ResourceFactory.createProperty(URI + "numberOfPages")
-}
