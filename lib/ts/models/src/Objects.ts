062dfae5779fc0eaede09e0da17d94331884f20d
Minor G <minorg@server.fake>
2020-09-22 19:19:34 -0400

    Objects.indexByCollectionUri

--- lib/ts/models/src/Objects.ts
+++ lib/ts/models/src/Objects.ts
@@ -135,6 +135,22 @@ export class Objects {
     return objects;
   }
 
+  static indexByCollectionUri(kwds: {
+    objects: readonly Object[];
+  }): {[index: string]: readonly Object[]} {
+    const result: {[index: string]: Object[]} = {};
+    for (const object of kwds.objects) {
+      for (const collectionUri of object.collectionUris) {
+        let collectionObjects = result[collectionUri];
+        if (!collectionObjects) {
+          result[collectionUri] = collectionObjects = [];
+        }
+        collectionObjects.push(object);
+      }
+    }
+    return result;
+  }
+
   static join(kwds: {
     collectionsByUri: {[index: string]: Collection};
     imagesByDepictsUri: {[index: string]: readonly Image[]};
