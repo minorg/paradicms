8b135c268d99e05e0dffd686962590a05edbdab3
Minor G <minorg@server.fake>
2020-10-27 19:30:04 -0400

    make most fields full-text searchable

--- lib/ts/lunr/src/ObjectIndex.ts
+++ lib/ts/lunr/src/ObjectIndex.ts
@@ -1,18 +1,57 @@
 import lunr, {Index} from "lunr";
-import {Object} from "@paradicms/models";
+import {Object, PropertyDefinition} from "@paradicms/models";
+import {encodeFileName} from "@paradicms/base";
 
 export class ObjectIndex {
   private readonly index: Index;
   private readonly objectsByUri: {[index: string]: Object};
 
-  constructor(objects: readonly Object[]) {
+  constructor(
+    objects: readonly Object[],
+    propertyDefinitions: readonly PropertyDefinition[]
+  ) {
+    const fieldsByPropertyDefinitionUri = propertyDefinitions.reduce(
+      (fieldsByPropertyDefinitionUri, propertyDefinition) => {
+        if (propertyDefinition.fullTextSearchable) {
+          fieldsByPropertyDefinitionUri[propertyDefinition.uri] = {
+            propertyDefinition,
+            name: encodeFileName(propertyDefinition.uri),
+          };
+        }
+        return fieldsByPropertyDefinitionUri;
+      },
+      {} as {
+        [index: string]: {
+          name: string;
+          propertyDefinition: PropertyDefinition;
+        };
+      }
+    );
+
     const objectsByUri: {[index: string]: Object} = {};
     this.objectsByUri = objectsByUri;
+
     this.index = lunr(function() {
       this.field("title");
+      for (const field of Object.values(fieldsByPropertyDefinitionUri)) {
+        this.field(field.name);
+      }
       this.ref("uri");
       for (const object of objects) {
-        this.add({title: object.title, uri: object.uri});
+        const doc: any = {title: object.title, uri: object.uri};
+        if (object.properties && object.properties.length > 0) {
+          for (const objectProperty of object.properties) {
+            const field =
+              fieldsByPropertyDefinitionUri[
+                objectProperty.propertyDefinitionUri
+              ];
+            if (!field) {
+              continue;
+            }
+            doc[field.name] = objectProperty.value;
+          }
+        }
+        this.add(doc);
         objectsByUri[object.uri] = object;
       }
     });
