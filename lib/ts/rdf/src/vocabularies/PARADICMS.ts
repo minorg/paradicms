f9b4433da0e53233b7b5023b1c3b53ccba72e7fc
Minor G <minorg@server.fake>
2021-02-27 19:12:39 -0500

    GuiMetadata

--- lib/ts/rdf/src/vocabularies/PARADICMS.ts
+++ lib/ts/rdf/src/vocabularies/PARADICMS.ts
@@ -7,10 +7,13 @@ export class PARADICMS {
   static collection = PARADICMS.NS("collection");
   static faceted = PARADICMS.NS("faceted");
   static fullTextSearchable = PARADICMS.NS("fullTextSearchable");
+  static guiDocumentTitle = PARADICMS.NS("guiDocumentTitle");
+  static guiNavbarTitle = PARADICMS.NS("guiNavbarTitle");
   static object = PARADICMS.NS("object");
 
   // Resources
   static Collection = PARADICMS.NS("Collection");
+  static GuiMetadata = PARADICMS.NS("GuiMetadata");
   static Institution = PARADICMS.NS("Institution");
   static PropertyDefinition = PARADICMS.NS("PropertyDefinition");
 }
